3f5f5b082aaf095a5338dd01f49b4de20e445d88
OZ <osler.villegas@gmail.com>
2021-12-03 19:09:35 -0600

    primer push

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+	let htmlPath = path.resolve(__dirname, './views/home.html');
+	res.sendFile(htmlPath);
+});
+
+app.get('/register', (req, res) => {
+	let htmlPath = path.resolve(__dirname, './views/register.html');
+	res.sendFile(htmlPath);
+});
+
+app.get('/login', (req, res) => {
+	let htmlPath = path.resolve(__dirname, './views/login.html')
+	res.sendFile(htmlPath);
+});
+
+app.listen(3000, () => console.log('Server Running on 3000'));
